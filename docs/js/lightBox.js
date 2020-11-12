5cfc49080defd1e9c793cac165ee7826213dd97a
calciferDeLioncourt <jimsamzara@gmail.com>
2020-11-12 00:20:51 -0600

    update

--- docs/js/lightBox.js
+++ docs/js/lightBox.js
@@ -17,81 +17,91 @@ console.log('%c If you have to ask, you\'ll never know. If you know, you need on
 console.log('%c Gallery Light Box by https://fb.me/jizradesign','background: #000; color: #fff');
 // >>>>> -->>>>> ----- estructura de galleria ----- <<<<<-- <<<<<
 
-const contGall = document.querySelector("#jd-cont__gallery");
 const imgFront = document.querySelectorAll(".jd-front__img__gallery");
-
-function wrapInner(parent, wrapper, attribute, attributevalue) {
-    if(typeof wrapper === "string") {
-        wrapper = parent.appendChild(document.createElement(wrapper));
-        wrapper.setAttribute(attribute, attributevalue);
+class Gallery{
+    constructor(imagen, i){
+        this._imagen = imagen,
+        this._item = i;
     };
-};
 
-for(let i = 0; i < imgFront.length; i++){
-    if(imgFront[i].hasAttribute("src")){
-        wrapInner(contGall, 'section', 'class', `jd-front__item__gallery`);
-        let jdGallery = document.querySelectorAll('.jd-front__item__gallery');
-        let img = jdGallery[i].appendChild(document.createElement('img'));
-            img.setAttribute('loading', 'lazy');
-            fetch(imgFront[i].src)
-            .then(res => res.blob())
-            .then(data => {
-                let imgen = URL.createObjectURL(data);
-                img.setAttribute('src', imgen);
-            });
-            if(imgFront[i].dataset.urlImg != ""){
-                fetch(imgFront[i].dataset.urlImg)
+    crearGallery(){
+        if(this._imagen.hasAttribute("src")){
+            const contGall = document.querySelector("#jd-cont__gallery");
+            this.wrapInner(contGall, 'section', 'class', `jd-front__item__gallery`);
+            let jdGallery = document.querySelectorAll('.jd-front__item__gallery');
+            let img = jdGallery[this._item].appendChild(document.createElement('img'));
+                img.setAttribute('loading', 'lazy');
+                fetch(this._imagen.src)
                 .then(res => res.blob())
                 .then(data => {
                     let imgen = URL.createObjectURL(data);
-                    img.setAttribute('data-url-img', imgen);
+                    img.setAttribute('src', imgen);
                 });
-            }else{
-                img.setAttribute('data-url-img',"");
-            };
-            img.setAttribute('data-title-img', imgFront[i].dataset.titleImg);
-            img.setAttribute('data-desc-img', imgFront[i].dataset.descImg);
-            img.setAttribute('alt', imgFront[i].alt);
-        imgFront[i].remove();
+                if(this._imagen.dataset.urlImg != ""){
+                    fetch(this._imagen.dataset.urlImg)
+                    .then(res => res.blob())
+                    .then(data => {
+                        let imgen = URL.createObjectURL(data);
+                        img.setAttribute('data-url-img', imgen);
+                    });
+                }else{
+                    img.setAttribute('data-url-img',"");
+                };
+                img.setAttribute('data-title-img', this._imagen.dataset.titleImg);
+                img.setAttribute('data-desc-img', this._imagen.dataset.descImg);
+                img.setAttribute('alt', this._imagen.alt);
+            this._imagen.remove();
+        };
     };
+
+    wrapInner(parent, wrapper, attribute, attributevalue) {
+        if(typeof wrapper === "string") {
+            wrapper = parent.appendChild(document.createElement(wrapper));
+            wrapper.setAttribute(attribute, attributevalue);
+        };
+    };
+    
 };
+
+imgFront.forEach( (img, i) => {
+    let newImagen = new Gallery(img, i);
+    newImagen.crearGallery();
+});
 
 // >>>>> -->>>>> ----- light box ----- <<<<<-- <<<<<
-
-let lightBoxPadre = document.querySelector('body').appendChild(document.createElement('section'));
-lightBoxPadre.setAttribute("id","jd-light-box");
-lightBoxPadre.setAttribute("class","center");
-
-const imgGallery=document.querySelectorAll('.jd-front__item__gallery');
-const lightBox=document.querySelector('#jd-light-box');
-
-for(let i=0;i<imgGallery.length;i++){
-    imgGallery[i].addEventListener('click',()=>{
-        setTimeout(function(){
-            lightbox(i);
-        },300);
-    });
-};
-function lightbox(i){
-    this.i=i;
-    let lightBoxCont=document.createElement('section');
-    let dataUrl=document.querySelectorAll('.jd-front__item__gallery img')[i].dataset.urlImg;
-    let dataTitle=document.querySelectorAll('.jd-front__item__gallery img')[i].dataset.titleImg;
-    let dataDesc=document.querySelectorAll('.jd-front__item__gallery img')[i].dataset.descImg;
-    let ubicacionImg="";
-    
-    if(dataUrl!==""){
-        ubicacionImg=dataUrl;
-    }else{
-        ubicacionImg=document.querySelectorAll('.jd-front__item__gallery img')[i].src;
+class LightBox{
+    constructor(imagen, i){
+        this._imagen = imagen,
+        this._item = i;
     };
-    
-    let panel=` <div id="btn-clouse-lbox"class="center"title="Cerrar"><i class="far fa-times-circle"></i></div>
+
+    lightBox(){
+
+        let lightBoxPadre = document.querySelector('body').appendChild(document.createElement('section'));
+            lightBoxPadre.setAttribute("id","jd-light-box");
+            lightBoxPadre.setAttribute("class","center");
+
+        const lightBox = document.querySelector('#jd-light-box');
+
+
+        let lightBoxCont = document.createElement('section'),
+            dataUrl = this._imagen.dataset.urlImg,
+            dataTitle = this._imagen.dataset.titleImg,
+            dataDesc = this._imagen.dataset.descImg,
+            ubicacionImg = "";
+        
+        if(dataUrl!==""){
+            ubicacionImg=dataUrl;
+        }else{
+            ubicacionImg=this._imagen.src;
+        };
+
+        let panel=` <div id="btn-clouse-lbox"class="center"title="Cerrar"><i class="far fa-times-circle"></i></div>
                     <img id="img-panel"src="${ubicacionImg}">
                     <div class="descripcion">
                         <p class="data-title">${dataTitle}</p>
                         <p class="data-desc">${dataDesc}</p>
-                        <p class="data-cont">Imagen ${i+1} de ${imgGallery.length}</p>
+                        <p class="data-cont">Imagen ${this._item+1} de ${imgGallery.length}</p>
                         <span class="e-firm">Gallery by <a href="https://jizratest.eu5.org">Jizra</a></span>
                     </div>
                     <div id="derecha"class="flecha"title="Derecha"><i class="fas fa-chevron-right"></i></div>
@@ -118,76 +128,92 @@ function lightbox(i){
                         </section>
                     </setion>`;
 
-    if(!document.querySelector('#jd-light-box-cont')){
-        lightBox.classList.add('active');
-        lightBoxCont.setAttribute("id","jd-light-box-cont");
-        lightBoxCont.setAttribute("class","jd-light-box-cont center");
-        lightBoxCont.innerHTML=panel;
-        lightBox.appendChild(lightBoxCont);
+        if(!document.querySelector('#jd-light-box-cont')){
 
-        let imgPanel=document.querySelector('#img-panel');
-        imgPanel.addEventListener('load',()=>{
-            document.querySelector('.cont__pre-loader').style.display='none';
-            setTimeout(function(){
-                document.querySelector('#img-panel').classList.add('active');
-                document.querySelector('.descripcion').style.width = '100%';
-                let flecha = document.querySelectorAll('.flecha');
-                flecha.forEach(flechaI => {
-                    flechaI.style.display = 'flex';
-                })
-            },300);
-        });
+            lightBox.classList.add('active');
+            lightBoxCont.setAttribute("id","jd-light-box-cont");
+            lightBoxCont.setAttribute("class","jd-light-box-cont center");
+            lightBoxCont.innerHTML=panel;
+            lightBox.appendChild(lightBoxCont);
+
+            let imgPanel=document.querySelector('#img-panel');
+            imgPanel.addEventListener('load',()=>{
+                document.querySelector('.cont__pre-loader').style.display='none';
+                setTimeout(function(){
+                    document.querySelector('#img-panel').classList.add('active');
+                    document.querySelector('.descripcion').style.width = '100%';
+                    let flecha = document.querySelectorAll('.flecha');
+                    flecha.forEach(flechaI => {
+                        flechaI.style.display = 'flex';
+                    })
+                },300);
+            });
+            
+            document.querySelector('#btn-clouse-lbox').addEventListener('click',()=>{
+                this.borrarlightbox();
+            });
+            document.querySelector('#derecha').addEventListener('click',()=>{
+                if(this._imagen.parentNode.nextElementSibling){
+                    this.avance(this._imagen.parentNode.nextElementSibling.querySelector('img'),this._item + 1 );
+                };
+            });
+            document.querySelector('#izquierda').addEventListener('click',()=>{
+                if(this._imagen.parentNode.previousElementSibling){
+                    this.avance(this._imagen.parentNode.previousElementSibling.querySelector('img'),this._item - 1);
+                };
+            });
+            window.addEventListener('keyup', e => {    
+                this.teclas(e);
+            });
+        };
+
+        let letra = document.querySelectorAll('.loader__texto__span');
         
-        document.querySelector('#btn-clouse-lbox').addEventListener('click',()=>{
-            borrarlightbox();
-        });
-        document.querySelector('#derecha').addEventListener('click',()=>{
-            avance(+1);
-        });
-        document.querySelector('#izquierda').addEventListener('click',()=>{
-            avance(-1);
-        });
-        window.addEventListener('keyup', e=>{    
-            teclas(e);
-        });
+        for(let i = 0; i < letra.length; i++){
+            letra[i].style = `animation: texto 2s ease-in-out ${0.1 * i}s infinite;`
+        };
+
     };
-    let letra = document.querySelectorAll('.loader__texto__span');
-    for(let i = 0; i < letra.length; i++){
-        letra[i].style = `animation: texto 2s ease-in-out ${0.1 * i}s infinite;`
-    }
-};
 
-// >>>>> -->>>>> ----- controles ----- <<<<<-- <<<<<
+    // >>>>> -->>>>> ----- controles ----- <<<<<-- <<<<<
     
-function borrarlightbox(){
-    lightBox.classList.remove('active');
-    setTimeout(function(){
-        document.querySelector('#jd-light-box-cont').remove();
-    },500);
-};
-
-function avance(x){
-    this.x=x;
-    let res = i + x;
-    borrarlightbox();
-    if(imgGallery[res]){
-        setTimeout(function(){
-            lightbox(res);
+    borrarlightbox(){
+        const lightBox = document.querySelector('#jd-light-box');
+        lightBox.classList.remove('active');
+        setTimeout(() => {
+            document.querySelector('#jd-light-box-cont').remove();
         },500);
     };
-    //console.log(res);
+
+    avance(element, i){
+        let res = element;
+        this.borrarlightbox();
+        if(res){
+            setTimeout(() => {
+                let imagen = new LightBox(res, i);
+                imagen.lightBox();
+            }, 500);
+        };
+    };
+
+    teclas(e){    
+        if(e.keyCode==27){
+            this.borrarlightbox();
+        };
+    };
+
+
 };
 
-function teclas(e){    
-    if(e.keyCode==27){
-        borrarlightbox();
-    }/*else if(e.keyCode==39){
-        avance(+1);
-    }else if(e.keyCode==37){
-        avance(-1);
-    };
-    console.log(e.keyCode);*/
-};
+const imgGallery = document.querySelectorAll('.jd-front__item__gallery');
+
+imgGallery.forEach((img, i) => {
+    img.addEventListener("click", e => {
+        let imagen = new LightBox(e.target, i);
+            imagen.lightBox();
+    });
+});
+
 
 // >>>>> -->>>>> ----- animaciones ----- <<<<<-- <<<<<
 
